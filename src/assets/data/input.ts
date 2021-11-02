945e3b0b790c0cebb32c50f5b7a230c2c64de2be
oligen <61948998+oligen@users.noreply.github.com>
2021-11-02 22:58:41 +0400

    Add files via upload

---
+++ src/assets/data/input.ts
@@ -0,0 +1,15 @@
+export const inputData = {
+    plateau: {
+        upperRightCoordinate: "3,3"
+    },
+    rover: [
+        {
+            position: "0 0 N",
+            instructions: "M, M, M, M, M, M"
+        },
+        {
+            position: "1 0 S",
+            instructions: "M, L, M, R, M, M"
+        }
+    ]
+};
